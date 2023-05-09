5d688d77af797db17654ad9ca34b1e8d7dae6068
ALVARO MARTINEZ DEL CUVILLO <ALX@SOOLOGIC.local>
2023-05-09 11:41:36 +0200

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/components/counter.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,10 +7,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
